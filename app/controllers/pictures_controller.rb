228ac76a2c3af1376709e9f2eab2bfadc66e44f6
hiro <hironori.matsuse@gmail.com>
2019-10-01 14:29:33 +0900

    Add:一対多のアソシエーションの実装

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -9,7 +9,7 @@ class PicturesController < ApplicationController
   end
 
   def create
-    @picture = Picture.new(picture_params)
+    @picture = current_user.pictures.build(picture_params)
     if params[:back]
       render :new
     else
@@ -35,7 +35,8 @@ class PicturesController < ApplicationController
   end
 
   def confirm
-    @picture = Picture.new(picture_params)
+    @picture = current_user.pictures.build(picture_params)
+    render :new if @blog.invalid?
   end
 
 
