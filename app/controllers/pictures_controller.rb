2d42457188edf3432aef9d3c0f879e330efbb102
hiro <hironori.matsuse@gmail.com>
2019-10-01 21:47:01 +0900

    Add,Fix:画像アップローダー機能とバグ修正

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -5,7 +5,11 @@ class PicturesController < ApplicationController
   end
 
   def new
-    @picture = Picture.new
+    if params[:back]
+      @picture = Picture.new(picture_params)
+    else
+      @picture = Picture.new
+    end
   end
 
   def create
@@ -31,23 +35,23 @@ class PicturesController < ApplicationController
 
   def destroy
     @picture.destroy
-    redirect_to pictures_path, notice: "ブログを削除しました"
+    redirect_to pictures_path, notice: "投稿を削除しました"
   end
 
   def confirm
-    @picture = current_user.pictures.build(picture_params)
-    render :new if @blog.invalid?
+    @picture = Picture.new(picture_params)
+    render :new if @blog
   end
 
   def show
-    @favorite = current_user.favorites.find_by(picture_id: @picture.id)
+    # @favorite = Favorite.new, current_user.favorites.find_by(picture_id: @picture.id)
   end
 
 
   private
 
   def picture_params
-    params.require(:picture).permit(:content)
+    params.require(:picture).permit(:content, :image, :image_cache)
   end
 
   def set_picture
