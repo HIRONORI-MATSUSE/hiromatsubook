d1ad2bc6713f43041829e6b2b63c8189eb5eecb5
hiro <hironori.matsuse@gmail.com>
2019-10-01 11:06:51 +0900

    Add:共通処理とパーシャル作成

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,4 +1,5 @@
 class PicturesController < ApplicationController
+  before_action :set_picture, only: [:show, :edit, :update]
   def index
     @pictures = Picture.all
   end
@@ -8,7 +9,7 @@ class PicturesController < ApplicationController
   end
 
   def create
-    Picture.create(picture_params)
+    @picture = Picture.new(picture_params)
     if @picture.save
       redirect_to pictures_path, notice: "ブログを作成しました"
     else
@@ -16,13 +17,22 @@ class PicturesController < ApplicationController
     end
   end
 
-  def show
-    @picture =Picture.find(params[:id])
+  def update
+    if @picture.update(picture_params)
+      redirect_to pictures_path, notice:"投稿を編集しました"
+    else
+      render :edit
+    end
   end
+
 
   private
 
   def picture_params
     params.require(:picture).permit(:content)
+  end
+
+  def set_picture
+    @picture = Picture.find(params[:id])
   end
 end
--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,3 +1,3 @@
 class Picture < ApplicationRecord
-  validates :title, presence: true
+  validates :content, presence: true
 end
